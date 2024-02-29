fe3cb7bebdc023ab6388e70997ca78fe7b3f89d9
Yeffer Moreno Robledo <yefire.js@gmail.com>
2024-02-29 10:52:57 -0500

    upd

--- Poke V.2/logic/main.js
+++ Poke V.2/logic/main.js
@@ -1,8 +1,8 @@
-const PokemonList = document.querySelector("#listaPokemon");
+const listwrapper = document.querySelector("#listaPokemon");
 const apiUrl = "https://pokeapi.co/api/v2/pokemon/";
-const pokemonlista = [];
+const pokemonlista=[];
 
-fetch(`${apiUrl}?limit=180`)
+fetch(`${apiUrl}?limit=3`)
   .then((response) => response.json())
   .then((data) => {
     const pokemons = data.results;
@@ -10,7 +10,8 @@ fetch(`${apiUrl}?limit=180`)
       fetch(pokemon.url)
         .then((response) => response.json())
         .then((pokemonData) => {
-            // Create stats Data Pokemon
+          
+          // Create stats Data Pokemon
           let objetoStatsPokemon = {};
            pokemonData.stats.forEach((e) => {
             objetoStatsPokemon[e.stat.name] = e.base_stat;
@@ -26,6 +27,9 @@ fetch(`${apiUrl}?limit=180`)
             types: pokemonData.types.map((e) => e.type.name),
             stats: objetoStatsPokemon,
           });
+          
+          
+          // BuiltPokemon(pokemonlista)
         })
         .catch((error) => {
           console.error(
@@ -36,4 +40,56 @@ fetch(`${apiUrl}?limit=180`)
     });
   });
 
-console.log(pokemonlista);
+  
+  function BuiltPokemon(ListAllPokemon) {
+    listwrapper.innerHTML = "";
+
+    ListAllPokemon.forEach((pokemon) => {
+      const cardPokemon = document.createElement("div");
+      cardPokemon.className = "pokemon";
+      cardPokemon.innerHTML = `
+      <div class="number-wrap">
+          <p class="pokemon-id-back">${pokemon.order}</p>
+          <div class="pokemon-imagen">
+              <img src="${pokemon.img}" alt="Pikachu"
+                   alt="${pokemon.name}">
+          </div>
+          <div class="pokemon-info">
+              <div class="nombre-contenedor">
+                  <p class="pokemon-id">#${pokemon.order}</p>
+                  <h2 class="pokemon-nombre">${pokemon.name}</h2>
+              </div>
+              <div class="pokemon-tipos">
+              
+                  <p class="electric tipo">ELECTRIC</p>
+                  <p class="fighting tipo">FIGHTING</p>
+              </div>
+              <div class="pokemon-stats">
+                  <p class="stat">4m</p>
+                  <p class="stat">60kg</p>
+              </div>
+          </div>
+      </div>
+      `;
+
+
+  
+      listwrapper.appendChild(cardPokemon);
+  
+    });
+    
+  }
+
+
+
+
+  async function ejecucionAsincronica() {
+    await new Promise(resolve => setTimeout(resolve,3000));  
+    pokemonlista.forEach(e=>{
+      console.log(e);
+    })
+  
+  }
+
+
+  ejecucionAsincronica()
