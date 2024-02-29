57ab0a02d4f3837aa64b8888ace1ecfca95a9cf8
Yeffer Moreno Robledo <yefire.js@gmail.com>
2024-02-28 22:16:13 -0500

    Update main.js

--- Poke V.2/logic/main.js
+++ Poke V.2/logic/main.js
@@ -1,30 +1,34 @@
 const PokemonList = document.querySelector("#listaPokemon");
 const apiUrl = "https://pokeapi.co/api/v2/pokemon/";
-const pokemonlist = [];
+const pokemonlista = [];
 
-
-fetch(`${apiUrl}?limit=10`)
-  .then(response => response.json())
-  .then(data => {
+fetch(`${apiUrl}?limit=3`)
+  .then((response) => response.json())
+  .then((data) => {
     const pokemons = data.results;
-    pokemons.forEach(pokemon => {
+    pokemons.forEach((pokemon) => {
       fetch(pokemon.url)
-        .then(response => response.json())
-        .then(pokemonData => {
-          
-            PokemonList.push(
-              {
-                name:pokemonData.name,
-                type:pokemonData.types[0].type.name,
-                img:pokemonData.sprites.front_default
-              }
-              
-              ) 
-
-           
+        .then((response) => response.json())
+        .then((pokemonData) => {
+          console.log(pokemonData);
+          pokemonlista.push({
+            order: pokemonData.order,
+            name: pokemonData.name,
+            type: pokemonData.types[0].type.name,
+            img: pokemonData.sprites.other["official-artwork"].front_default,
+            weight: pokemonData.weight,
+            height: pokemonData.height,
+            types: pokemonData.types.map(e=>e.type.name)
+          });
         })
-        .catch(error => {
-          console.error('Hubo un error al obtener los datos del Pokémon:', error);
+        .catch((error) => {
+          console.error(
+            "Hubo un error al obtener los datos del Pokémon:",
+            error
+          );
         });
     });
-  })
+  });
+
+
+  console.log(pokemonlista);
