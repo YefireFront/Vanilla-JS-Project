738978e4a08278d4b687227d1759f7e14bf18c8a
Yeffer Moreno Robledo <yefire.js@gmail.com>
2024-02-28 21:06:18 -0500

    Update main.js

--- Poke V.2/logic/main.js
+++ Poke V.2/logic/main.js
@@ -3,13 +3,28 @@ const apiUrl = "https://pokeapi.co/api/v2/pokemon/";
 const pokemonlist = [];
 
 
-for (let i = 0; i < 15; i++) {
-    fetch(apiUrl + 1)
-    .then(res=>res.json())
-    .then(data=>showPokemon(data))
-    
-}
+fetch(`${apiUrl}?limit=10`)
+  .then(response => response.json())
+  .then(data => {
+    const pokemons = data.results;
+    pokemons.forEach(pokemon => {
+      fetch(pokemon.url)
+        .then(response => response.json())
+        .then(pokemonData => {
+          
+            PokemonList.push(
+              {
+                name:pokemonData.name,
+                type:pokemonData.types[0].type.name,
+                img:pokemonData.sprites.front_default
+              }
+              
+              ) 
 
-function showPokemon(data) {
-
-}
+           
+        })
+        .catch(error => {
+          console.error('Hubo un error al obtener los datos del Pokémon:', error);
+        });
+    });
+  })
