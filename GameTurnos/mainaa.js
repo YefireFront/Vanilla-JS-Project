280ff9940a66390297a4b756fc8853f9d1146d69
Yeffer Moreno Robledo <98995975+YefireFront@users.noreply.github.com>
2024-09-09 17:53:42 -0500

    Create mainaa.js

---
+++ GameTurnos/mainaa.js
@@ -0,0 +1,45 @@
+
+
+const yeffer = {
+    nombre : 'yeffer',
+    vida : 100,
+    ataque : 40,
+    tipo : 'Bestia',
+    efectos : ['quemado' , 'congelado'],
+
+}
+
+
+const tert = {
+    nombre : 'yeffer',
+    vida : 100,
+    ataque : 40,
+    tipo : 'Bestia',
+    efectos : ['quemado' , 'congelado'],
+
+}
+
+
+const kak = {
+    nombre : 'yeffer',
+    vida : 100,
+    ataque : 40,
+    tipo : 'Bestia',
+    efectos : ['quemado' , 'congelado'],
+
+}
+
+
+const juyg = [yeffer. tert, kak]
+
+
+
+// Limpiar el perimer efecto
+
+// Ordenarlos por ataque
+
+// Eliminar un personaje si la vida es 0
+
+// aumentar el ataque de un tipo bestia por cada tipo hada en el campo
+
+
