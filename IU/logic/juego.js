2826d16046a429605a8eff6df2086b3d0fb8d4c2
Yeffer Moreno Robledo <yefire.js@gmail.com>
2024-09-10 22:00:40 -0500

    Update juego.js

--- IU/logic/juego.js
+++ IU/logic/juego.js
@@ -80,5 +80,11 @@ class Juego {
 
 
 
+Juego.agregarPersonaje(1, darkOz);
+Juego.agregarPersonaje(1, skells);
+Juego.agregarPersonaje(1, Valvius);
 
+Juego.agregarPersonaje(2, skells);
+Juego.agregarPersonaje(2, darkOz);
+Juego.agregarPersonaje(2, Valvius);
 
