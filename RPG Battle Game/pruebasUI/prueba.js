78bd06b4de6c2df4cf8b1b87fa0fa0c0d1c766fd
Yeffer Moreno Robledo <98995975+YefireFront@users.noreply.github.com>
2024-09-25 12:45:05 -0500

    upc

---
+++ RPG Battle Game/pruebasUI/prueba.js
@@ -0,0 +1,10 @@
+const cuerpo1 = document.querySelector('.esenario .esenarioEquipo1');
+const cuerpo2 = document.querySelector('.esenario .esenarioEquipo2');
+
+const destino1 = cuerpo1.querySelector('.personajeSecundario');
+const destino2 = cuerpo2.querySelector('.personajeSecundario');
+
+console.log(cuerpo1);
+console.log(destino1)
+console.log(cuerpo2);
+console.log(destino2)
