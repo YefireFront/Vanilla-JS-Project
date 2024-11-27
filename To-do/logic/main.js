777dc021d67b0a1d2a897f4ac38f42ace6104b66
Yeffer Moreno Robledo <yefire.js@gmail.com>
2024-11-26 23:09:55 -0500

    Update main.js

--- To-do/logic/main.js
+++ To-do/logic/main.js
@@ -11,34 +11,3 @@ filterOption.addEventListener("click", filterTodo);
 
 //*FUNCTION
 
-function addTodo(event) {
-  event.preventDefault();
-
-  //*Creating DIV todo
-  const todoDiv = document.createElement("div");
-  todoDiv.classList.add("todo");
-
-  //*Creating Li todo
-  const todoLi = document.createElement("li");
-  todoLi.innerText = todoInput.value;
-  todoLi.classList.add("todo-item");
-  todoDiv.appendChild(todoLi);
-
-  //*Checked button
-  const CheckedButtond = document.createElement("button");
-  CheckedButtond.innerHTML = '<i class="fas fa-check"></i>';
-  CheckedButtond.classList.add("Checked-btn");
-  todoDiv.appendChild(CheckedButtond);
-
-  //*Delete button
-  const deleteButton = document.createElement("button");
-  deleteButton.innerHTML = '<i class="fas fa-trash"></i>';
-  deleteButton.classList.add("complete-btn");
-  todoDiv.appendChild(deleteButton);
-
-  //*APPEND
-  todoList.append(todoDiv);
-
-  todoInput.value = "";
-}
-
