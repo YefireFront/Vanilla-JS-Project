19cbe5fd7cc46cc7e22bad81e040b46a95de5c46
Yeffer Moreno Robledo <98995975+YefireFront@users.noreply.github.com>
2024-12-04 23:45:23 -0500

    Update parallax.js

--- TeachVideoGame/parallax.js
+++ TeachVideoGame/parallax.js
@@ -1,1 +1,11 @@
-    
+document.addEventListener('mousemove', parallax);
+
+function parallax(e) {
+    // Calcula el porcentaje del ancho y alto
+    let width = window.innerWidth / 2;
+    let height = window.innerHeight / 2;
+
+    // Obtén las posiciones del mouse relativas al centro
+    let mouseX = (e.clientX - width) / width;
+    let mouseY = (e.clientY - height) / height;
+
